7801bb7a4f3a79810f8cb846b16f515c9eebdda2
Deepak Vishwakarma <coder.deepak44@gmail.com>
2024-04-06 15:21:04 +0530

    Delected

--- News App/vbb.java
+++
@@ -1,8 +0,0 @@
-class animal {
-    private string str  a = 10;
-    public static void main(String[] args) {
-        
-        animal obj = new animal();
-        System.out.println(obj.str);
-    }
-}
